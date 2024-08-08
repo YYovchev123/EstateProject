725702703c5cc8dc66b547422263f6c70907f718
YYovchev123 <jonimi@abv.bg>
2024-08-08 13:51:23 +0300

    commit

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -2,5 +2,53 @@
 pragma solidity 0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
+import {EstateProject} from "../../src/EstateProject.sol";
 
-contract Handler is Test {}
+contract Handler is Test {
+    EstateProject estateProject;
+    address[] investors;
+    mapping(uint256 invetor => uint256 amount) investorToAmount;
+    address investorOne = makeAddr("InvestorOne");
+    address investorTwo = makeAddr("investorTwo");
+    address investorThree = makeAddr("investorThree");
+    address investorFour = makeAddr("investorFour");
+    address investorFive = makeAddr("investorFive");
+
+    constructor(EstateProject _estateProject) {
+        investors.push(investorOne);
+        investors.push(investorTwo);
+        investors.push(investorThree);
+        investors.push(investorFour);
+        investors.push(investorFive);
+        estateProject = _estateProject;
+    }
+
+    function invest(uint256 amount, uint256 investor) public {
+        // if (amount + estateProject.getTotalAmountInvested() > estateProject.getTargetFundrasingAmount()) {
+        //     return;
+        // }
+        bound(investor, 0, 4);
+        bound(amount, 1, 20 ether);
+        address investorAddress = investors[investor];
+        vm.deal(investorAddress, amount);
+
+        investorToAmount[investor] += amount;
+
+        vm.prank(investorAddress);
+        estateProject.invest{value: amount}();
+    }
+
+    // function retreiveInvestment(uint256 investor, uint256 amount) public {
+    //     // if()
+    //     // vm.roll(100);
+    //     // vm.warp(10 days);
+    //     bound(investor, 0, 4);
+    //     bound(amount, 1, investorToAmount[investor]);
+
+    //     address investorAddress = investors[investor];
+    //     investorToAmount[investor] -= amount;
+
+    //     vm.prank(investorAddress);
+    //     estateProject.invest{value: amount}();
+    // }
+}
--- test/fuzz/Invariants.s.sol
+++ test/fuzz/Invariants.s.sol
@@ -3,5 +3,40 @@ pragma solidity 0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {EstateProject} from "../../src/EstateProject.sol";
+import {Handler} from "./Handler.t.sol";
 
-contract Invariants is StdInvariant, Test {}
+contract Invariants is StdInvariant, Test {
+    EstateProject estateProject;
+    Handler handler;
+
+    string name = "Estate Token";
+    string symbol = "EST";
+    string location = "Varna, Bulgaria";
+    uint256 deadline = block.timestamp + 5 days;
+    uint256 apartmentsAvailable = 3;
+    uint256 targetFundrasingAmount = 20 ether;
+    uint256[] apartmentPrices;
+
+    function setUp() external {
+        apartmentPrices.push(8 ether);
+        apartmentPrices.push(15 ether);
+        apartmentPrices.push(18 ether);
+
+        estateProject = new EstateProject(
+            name, symbol, location, deadline, apartmentsAvailable, targetFundrasingAmount, apartmentPrices
+        );
+        handler = new Handler(estateProject);
+        targetContract(address(handler));
+    }
+
+    // This should only hold when the deadline has not passed
+    function invariant_totalAmountInvestedShouldNotBeMoreThanTargetFundrasingAmount() public view {
+        uint256 _targetFundrasingAmount = estateProject.getTargetFundrasingAmount();
+        uint256 _totalAmountInvested = estateProject.getTotalAmountInvested();
+
+        assert(_totalAmountInvested <= _targetFundrasingAmount);
+    }
+
+    function invariant_retreiveInvestmentShouldOnlyBeCalledWhenDeadlineHasPassedAndTargetNotReached() public {}
+}
